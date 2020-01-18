cc24956e85f3667875afb1a61d4c54e894d81768
Jens W. Klein <jk@kleinundpartner.at>
2020-01-18 01:03:48 +0100

    add locales

--- sphinx_materialdesign_theme/__init__.py
+++ sphinx_materialdesign_theme/__init__.py
@@ -10,3 +10,9 @@ def get_path():
 
 def setup(app):
     app.add_html_theme('sphinx_materialdesign_theme', package_dir)
+
+    if sphinx.version_info >= (1, 8, 0):
+        # Add Sphinx message catalog for newer versions of Sphinx
+        # See http://www.sphinx-doc.org/en/master/extdev/appapi.html#sphinx.application.Sphinx.add_message_catalog
+        rtd_locale_path = path.join(get_path(), 'locale')
+        app.add_message_catalog('sphinx', rtd_locale_path)
---
+++ update_locales.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+pybabel extract -F babel-mapping.ini -o sphinx_materialdesign_theme/locale/sphinx.pot sphinx_materialdesign_theme
+pybabel update -D sphinx -i sphinx_materialdesign_theme/locale/sphinx.pot -d sphinx_materialdesign_theme/locale
