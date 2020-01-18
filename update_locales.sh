f966a93a4303d903f8a4285599dbad9990fc1ccd
Jens W. Klein <jk@kleinundpartner.at>
2020-01-18 11:51:03 +0100

    seiteninhalt, compile

--- update_locales.sh
+++ update_locales.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 pybabel extract -F babel-mapping.ini -o sphinx_materialdesign_theme/locale/sphinx.pot sphinx_materialdesign_theme
 pybabel update -D sphinx -i sphinx_materialdesign_theme/locale/sphinx.pot -d sphinx_materialdesign_theme/locale
+pybabel compile -D sphinx -d sphinx_materialdesign_theme/locale/
